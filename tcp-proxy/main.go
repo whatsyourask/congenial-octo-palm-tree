c0811747ee832501b20e651e43d83466f4174d1c
whatsyourask <lexeymartynov@gmail.com>
2022-08-30 00:34:09 +0300

    completed simple tcp proxy

--- tcp-proxy/main.go
+++ tcp-proxy/main.go
@@ -6,26 +6,36 @@ import (
 	"net"
 )
 
-func echo(conn net.Conn) {
-	defer conn.Close()
+func handle(src net.Conn) {
+	dst, err := net.Dial("tcp", "127.0.0.1:80")
+	if err != nil {
+		log.Fatalln("Unable to connect to our unreachable host")
+	}
+	defer dst.Close()
 
-	if _, err := io.Copy(conn, conn); err != nil {
-		log.Fatalln("Unable to read/write data")
+	go func() {
+		if _, err := io.Copy(dst, src); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	if _, err := io.Copy(src, dst); err != nil {
+		log.Fatalln(err)
 	}
 }
 
 func main() {
-	listener, err := net.Listen("tcp", ":20000")
+	listener, err := net.Listen("tcp", ":8888")
 	if err != nil {
 		log.Fatalln("Unable to bind to port")
 	}
-	log.Println("Listening on 0.0.0.0:20000")
+	log.Println("Listening on 0.0.0.0:8888")
 	for {
 		conn, err := listener.Accept()
 		log.Println("Received connection")
 		if err != nil {
 			log.Fatalln("Unable to accept connection")
 		}
-		go echo(conn)
+		go handle(conn)
 	}
 }
