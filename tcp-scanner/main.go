38ee0e73d8edfb793a48dd3ad4e8f656abf616b4
whatsyourask <lexeymartynov@gmail.com>
2022-08-29 23:48:42 +0300

    Remove WaitGroup and add new channel to store all results for future sort and output for user

--- tcp-scanner/main.go
+++ tcp-scanner/main.go
@@ -3,23 +3,47 @@ package main
 import (
 	"fmt"
 	"net"
-	"sync"
+	"sort"
 )
 
-func main() {
-	var wg sync.WaitGroup
-	for i := 1; i <= 1024; i++ {
-		wg.Add(1)
-		go func(port int) {
-			defer wg.Done()
-			address := fmt.Sprintf("scanme.nmap.org:%d", port)
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				return
-			}
-			conn.Close()
-			fmt.Printf("%d/tcp - open\n", port)
-		}(i)
+func worker(ports, results chan int) {
+	for port := range ports {
+		address := fmt.Sprintf("scanme.nmap.org:%d", port)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- port
 	}
-	wg.Wait()
+}
+
+func main() {
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openports []int
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+	close(ports)
+	close(results)
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d/tcp - open\n", port)
+	}
 }
